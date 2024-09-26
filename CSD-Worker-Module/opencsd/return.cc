34766a8b9fedffaebff77181c3dc8b97f90bfb07
juhyunkil <wngus0331@gmail.com>
2024-09-26 16:56:05 +0900

    make return struct

--- CSD-Worker-Module/opencsd/return.cc
+++ CSD-Worker-Module/opencsd/return.cc
@@ -13,7 +13,69 @@ void Return::send_data(Result &result){
 
     string json_;
 
+    StringBuffer block_buf;
+    PrettyWriter<StringBuffer> writer(block_buf);
+
+    writer.StartObject();
+
+    writer.Key("query_id");
+    writer.Int(result.snippet->query_id);
+
+    writer.Key("work_id");
+    writer.Int(result.snippet->work_id);
+
+    writer.Key("row_count");
+    writer.Int(result.data.row_count);
+
+    writer.Key("row_offset");
+    writer.StartArray();
+    for (int i = 0; i < result.data.row_offset.size(); i++){
+        writer.Int(result.data.row_offset[i]);
+    }
+    writer.EndArray();
+
+    writer.Key("length");
+    writer.Int(result.data.data_length);
+
+    writer.Key("return_column_type");
+    writer.StartArray();
+    for (int i = 0; i < result.snippet->result_info.return_column_type.size(); i++){
+        writer.Int(result.snippet->result_info.return_column_type[i]);
+    }
+    writer.EndArray();
+
+    writer.Key("return_column_length");
+    writer.StartArray();
+    for (int i = 0; i < result.snippet->result_info.return_column_length.size(); i++){
+        writer.Int(result.snippet->result_info.return_column_length[i]);
+    }
+    writer.EndArray();
+
+    writer.Key("current_block_count");
+    writer.Int(result.data.current_block_count);
+
+    // writer.Key("scannedRowCount");
+    // writer.Int(result.scanned_row_count);
+
+    // writer.Key("filteredRowCount");
+    // writer.Int(result.filtered_row_count);
+
+    writer.Key("column_alias");
+    writer.StartArray();
+    for (int i = 0; i < result.snippet->result_info.column_alias.size(); i++){
+        writer.String(result.snippet->result_info.column_alias[i].c_str());
+    }
+    writer.EndArray();
+
+    writer.Key("total_block_count");
+    writer.Int(result.snippet->result_info.total_block_count);
     
+    writer.Key("tableAlias");
+    writer.String(result.snippet->result_info.table_alias.c_str());
+
+    writer.EndObject();
+
+    string block_buf_ = block_buf.GetString();
 
     int sockfd;
     struct sockaddr_in serv_addr;
@@ -24,22 +86,13 @@ void Return::send_data(Result &result){
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(BUFF_M_PORT);
-
-    if(KETILOG::IsRunningMode(KETI_RUNNING_MODE::LOCAL)){
-        serv_addr.sin_addr.s_addr = inet_addr(STORAGE_ENGINE_LOCAL_IP);
-    }else{
-        serv_addr.sin_addr.s_addr = inet_addr(STORAGE_ENGINE_POD_IP);
-    }
+    serv_addr.sin_addr.s_addr = inet_addr(BUFF_M_IP);
 
     int response = connect(sockfd,(const sockaddr*)&serv_addr,sizeof(serv_addr));
     if( 0 != response ) {
         KETILOG::INFOLOG(LOGTAG, "[error] return interface connection failed");
     } 
-	
-	size_t len = strlen(json_.c_str());
-	send(sockfd, &len, sizeof(len), 0);
-    send(sockfd, (char*)json_.c_str(), len, 0);
-
+    
     static char cBuffer[PACKET_SIZE];
     if (recv(sockfd, cBuffer, PACKET_SIZE, 0) == 0){
         KETILOG::FATALLOG(LOGTAG,"client recv Error");
